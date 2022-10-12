5b3f5cdf515edbce9b865a73961b930b9a9a9ccc
Comrak <jnardulli@hotmail.com>
2022-10-12 18:17:25 -0300

    pagina de product details y estructura de datos

--- ViandasApp/app.js
+++ ViandasApp/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const PUBLICFOLDER = path.resolve('public')
+app.use(express.static(PUBLICFOLDER))
 const numeroPuerto = 3000;
 app.listen(numeroPuerto, ()=>{
     console.log('Servidor funcionando en el puerto ' + numeroPuerto);
 })
 app.get('/', (req, res)=>{
-    res.send(/views/home.html)
+    res.sendFile(path.resolve('views/home.html'))
+})
+app.get('/productDetails.html', (req, res)=>{
+    res.sendFile(path.resolve('views/productDetails.html'))
 })
