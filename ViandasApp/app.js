4166bc3a67ffba1feeb6796398eabe31fa5ea7ab
FlorenciaMontano <flormontano532@gmail.com>
2022-10-21 23:11:46 -0300

    login

--- ViandasApp/app.js
+++ ViandasApp/app.js
@@ -17,3 +17,7 @@ app.get('/productDetails', (req, res)=>{
 app.get('/register', (req, res)=>{
     res.sendFile(path.resolve('views/register.html'))
 })
+
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve('views/login.html'))
+})
