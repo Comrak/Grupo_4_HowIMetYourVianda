98ca82a246308448ffd8e1cac67ee278279ea442
FlorenciaMontano <flormontano532@gmail.com>
2022-10-23 22:33:07 -0300

    Correccion Login CSS

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PUBLICFOLDER = path.resolve('public')
+app.use(express.static(PUBLICFOLDER))
+const numeroPuerto = 3000;
+app.listen(numeroPuerto, ()=>{
+    console.log('Servidor funcionando en el puerto ' + numeroPuerto);
+})
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve('views/home.html'))
+})
+app.get('/productDetails', (req, res)=>{
+    res.sendFile(path.resolve('views/productDetails.html'))
+})
+
+app.get('/register', (req, res)=>{
+    res.sendFile(path.resolve('views/register.html'))
+})
+
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve('views/login.html'))
+})
+
+app.get('/carrito', (req, res)=>{
+    res.sendFile(path.resolve('views/carritoCompras.html'))
+})
+
+app.post('/carritoPost', (req, res)=>{
+    res.sendFile(path.resolve('views/home.html'))
+})
