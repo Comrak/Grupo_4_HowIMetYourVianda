17e72b5f298496f9e7b3a308d292ff613d1ce326
vickyaq <mariavictoriaaquino@gmail.com>
2022-11-14 20:53:54 -0300

    CRUD de creación y detalle de producto

--- ViandasApp/routers/productsRouter.js
+++ ViandasApp/routers/productsRouter.js
@@ -19,8 +19,8 @@ const productController = require('../controllers/productController');
 
 
 router.get('/', productController.renderProductDetails);
-router.get('/:id', productController.renderDetailId);
 router.get('/create', productController.renderProductManagement);
+router.get('/:id', productController.renderDetailId);
 router.get('/:id/edit', productController.renderProductEdit)
 router.post('/', upload.single("img"), productController.renderproductRegistration);
 
