49c6cae846b9ee88556d7a22d201914de1606394
Comrak <Jnardulli@hotmail.com>
2023-01-23 21:00:55 -0400

    Update Products.js
    arreglando repo

--- ViandasApp/database/models/Products.js
+++ ViandasApp/database/models/Products.js
@@ -3,11 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-<<<<<<< HEAD:ViandasApp/src/database/models/Products.js
-  class User extends Model {
-=======
   class Products extends Model {
->>>>>>> 952ce94a81e5d09bd94fb3b8f63bab8d359f1b8e:ViandasApp/database/models/Products.js
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -15,38 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-<<<<<<< HEAD:ViandasApp/src/database/models/Products.js
-    }
-
-  }; 
-     Products.init({
-      id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
-      },
-      image: {
-        type: DataTypes.STRING(255)
-      },
-      price: {
-          type: DataTypes.DECIMAL(20.2)
-      },
-      name: {
-          type: DataTypes.STRING(55)
-      },
-      description: {
-        type: DataTypes.STRING(255)
-      },
-      tags: {
-        type: DataTypes.STRING(255)
-      },
-  sequelize,
-      modelName: 'Products',
-      tableName: 'product'
-  });
-   return Products;
-}
-=======
       Products.belongsToMany(models.Users,{
         as:'productTransaction',
         through:'transaction',
@@ -69,4 +33,3 @@ module.exports = (sequelize, DataTypes) => {
   });
   return Products;
 };
->>>>>>> 952ce94a81e5d09bd94fb3b8f63bab8d359f1b8e:ViandasApp/database/models/Products.js
