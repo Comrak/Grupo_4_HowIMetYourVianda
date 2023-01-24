be02afc539812aaf78effcb6291390c37b928fa1
Comrak <Jnardulli@hotmail.com>
2023-01-23 21:01:27 -0400

    Update Users.js
    arreglando repo

--- ViandasApp/database/models/Users.js
+++ ViandasApp/database/models/Users.js
@@ -10,8 +10,6 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-<<<<<<< HEAD:ViandasApp/src/database/models/Users.js
-=======
       // define association here
       Users.belongsTo(models.UserRol,{
         as:'userRole',
@@ -25,38 +23,12 @@ module.exports = (sequelize, DataTypes) => {
         as:'userCountry',
         foreignKey:'country_id',
       }),
->>>>>>> 952ce94a81e5d09bd94fb3b8f63bab8d359f1b8e:ViandasApp/database/models/Users.js
       Users.belongsToMany(models.Products,{
         as:'userTransactions',
         through:'transaction',
         foreignKey:'user_id',
         otherKey:'product_id',
         timestamps:false
-<<<<<<< HEAD:ViandasApp/src/database/models/Users.js
-      })
-    }
-  }
-
-    Users.init({
-      fullName:DataTypes.STRING(55),
-      birthDate:DataTypes.DATE,
-      email:DataTypes.STRING(55),
-      mobilePhone:DataTypes.INTEGER,
-      address:DataTypes.STRING(255),
-      city:DataTypes.INTEGER,
-      country:DataTypes.INTEGER,
-      profile:DataTypes.INTEGER,
-      password:DataTypes.STRING(55),
-      avatar:DataTypes.STRING(255)
-    },{
-        sequelize,
-        modelName: 'Users',
-        tableName: 'user',
-  });
-    return Users;
-};
-
-=======
       }),
       Users.hasMany(models.Address,{
         as:'addresses',
@@ -84,4 +56,3 @@ module.exports = (sequelize, DataTypes) => {
   });
   return Users;
 };
->>>>>>> 952ce94a81e5d09bd94fb3b8f63bab8d359f1b8e:ViandasApp/database/models/Users.js
