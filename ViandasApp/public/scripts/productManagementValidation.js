bbe5037fd332f7ebf17f2110cf0044a651c1c426
FlorenciaMontano <flormontano532@gmail.com>
2023-01-25 17:24:27 -0300

    Validacion front Product Management

---
+++ ViandasApp/public/scripts/productManagementValidation.js
@@ -0,0 +1,103 @@
+
+//console.log('Funciona')
+
+const nameField = document.querySelector("[name=name]");
+// const priceField = document.querySelector("[name=price]");
+const descriptionField = document.querySelector("[name=description]");
+const imgField = document.querySelector("[name=img]");
+
+
+// Validacion de Nombre del Producto
+nameField.addEventListener("blur", function(e){
+    const field = e.target;
+    const fieldValue = e.target.value;
+    if (fieldValue.length < 5) {
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "Debes escribir el nombre del producto";
+    } else {
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+});
+
+
+
+// Validacion de Descripcion
+descriptionField.addEventListener("blur", function(e){
+    const field = e.target;
+    const fieldValue = e.target.value;
+    if (fieldValue.length < 20) {
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "Debes escribir una descripción del producto de un mínimo de 20 caracteres";
+    } else {
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+});
+
+// //Validacion de Email
+// //Expresiones Regulares https://www.w3schools.com/jsref/jsref_obj_regexp.asp       
+// //https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+// emailField.addEventListener("blur", function(e){
+//     const field = e.target;
+//     const fieldValue = e.target.value;
+//     let reEmail  = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+       
+//     if (fieldValue.length <= 2 && (!reEmail.test(emailField.value))) { 
+//         field.nextElementSibling.classList.add("error");
+//         field.nextElementSibling.innerText = "Debes escribir un email valido";
+//     } else {
+//         field.nextElementSibling.classList.remove("error");
+//         field.nextElementSibling.innerText = "";
+//     }
+// });
+
+// //Validacion contraseña
+// passwordField.addEventListener("blur", function(e){
+//     const field = e.target;
+//     const fieldValue = e.target.value;
+//     if (fieldValue.length <= 7) {
+//         field.nextElementSibling.classList.add("error");
+//         field.nextElementSibling.innerText = "Debes tener al menos 8 caracteres";
+//     } else {
+//         field.nextElementSibling.classList.remove("error");
+//         field.nextElementSibling.innerText = "";
+//     }
+// });
+
+
+//Validacion Imagen
+imgField.addEventListener("change", function(e){
+    const field = e.target;
+    const fileExtension = e.target.files[0].name.split(".").pop().toLowerCase();
+    const allowedExt = ["jpg", "jpeg", "png", "gif"];
+    if (!allowedExt.includes(fileExtension)) {
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = `Las extensiones permitidas son ${allowedExt.join(", ")}`;
+    } else {
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+  });
+
+
+
+
+
+// VALIDACIONES FRONT 
+
+//● Creación y modificación de productos
+
+//○ Nombre
+//■ Obligatorio.
+//■ Deberá tener al menos 5 caracteres.
+
+//○ Descripción
+//■ Deberá tener al menos 20 caracteres.
+
+//○ Imagen
+//■ Deberá ser un archivo válido (JPG, JPEG, PNG, GIF).
+
+//● (Opcional) Resto de los formularios del sitio
+
+
