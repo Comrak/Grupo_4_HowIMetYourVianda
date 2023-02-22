e8165b0e962184f58972e003aabcee3c63ffca58
Comrak <jnardulli@hotmail.com>
2023-02-22 19:01:39 -0400

    matando bugs

--- DashBoard/src/components/WidgetSmall/WidgetSmall.jsx
+++ DashBoard/src/components/WidgetSmall/WidgetSmall.jsx
@@ -15,24 +15,42 @@ export default function WidgetSmall() {
 
   const lastUser = userData.find(e => e.id = Math.max(...userData.map(elements => (elements.id))))
 
-  console.log(lastUser)
-  return (
-    <div className="widgetSmall">
-      <span className="widgetSmallTitle">Nuevos Miembros</span>
-      <ul className="widgetSmallList">
-        <li className="widgetSmallListItem">
-          <img
-            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-            alt=""
-            className="WidgetSmallImage"
-          />
-          <div className="widgetSmallUser">
-            <span className="widgetSmallUserName">{lastUser.fullName}</span>
-            <span className="widgetSmallUserTitle">{lastUser.email}</span>
-          </div>
-          <span className="widgetSmallUserTitle">{lastUser.mobilePhone}</span>
-        </li>
-      </ul>
-    </div>
-  );
+  if(lastUser != undefined){
+    return (
+      <div className="widgetSmall">
+        <span className="widgetSmallTitle">Nuevos Miembros</span>
+        <ul className="widgetSmallList">
+          <li className="widgetSmallListItem">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+              className="WidgetSmallImage"
+            />
+            <div className="widgetSmallUser">
+              <span className="widgetSmallUserName">{lastUser.fullName}</span>
+              <span className="widgetSmallUserTitle">{lastUser.email}</span>
+            </div>
+            <span className="widgetSmallUserTitle">{lastUser.mobilePhone}</span>
+          </li> 
+        </ul>
+      </div>
+    );
+  }else{
+    return (
+      <div className="widgetSmall">
+        <span className="widgetSmallTitle">Nuevos Miembros</span>
+        <ul className="widgetSmallList">
+          <li className="widgetSmallListItem">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+              className="WidgetSmallImage"
+            />
+            <p>no pude encontrar un usuario</p>
+          </li> 
+        </ul>
+      </div>
+    );
+  }
+  
 }
