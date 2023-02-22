10088f6dc2c06376b54af33d4cdf7208d425b545
ArielHL <73503229+ArielHL@users.noreply.github.com>
2023-02-22 10:35:49 -0300

    Update FeaturedInfo.jsx

--- DashBoard/src/components/FeaturedInfo/FeaturedInfo.jsx
+++ DashBoard/src/components/FeaturedInfo/FeaturedInfo.jsx
@@ -1,34 +1,70 @@
-import React from 'react'
+
+import React, { useState, useEffect } from 'react'
 import './FeaturedInfo.css'
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 export default function FeaturedInfo() {
+
+  const [userData, setUserData] = useState([])
+  const [productData, setProductData] = useState([])
+  const [addressData, setAddressData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3200/api/users")
+      .then((data) => data.json())
+      .then((data) => setUserData(data.data))
+
+  }, [])
+
+  useEffect(() => {
+    fetch("http://localhost:3200/api/products")
+      .then((data) => data.json())
+      .then((data) => setProductData(data.data))
+
+  }, [])
+
+  useEffect(() => {
+    fetch("http://localhost:3200/api/users/address")
+      .then((data) => data.json())
+      .then((data) => setAddressData(data.data))
+
+  }, [])
+
+
+  const userCount = userData.length
+  const productCount = productData.length
+  const addressCount = addressData.length
+ 
+
+
+
+
   return (
     <div className='featured'>
       <div className="featuredItems">
-        <span className="featuredTitle">Revenue</span>
+        <span className="featuredTitle">Cantidad Usuarios</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2.523</span>
+          <span className="featuredMoney">{userCount}</span>
           <span className="featuredMoneyRate">  -11.5 <ArrowDownwardIcon className='featureIcon negative'/> </span>
         </div>
-        <span className="featuredSub">Compared to last Month</span>
+        <span className="featuredSub">Comparado con el mes anterior</span>
       </div>
       <div className="featuredItems">
-        <span className="featuredTitle">Sales</span>
+        <span className="featuredTitle">Cantidad de Productos</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2.523.000</span>
+          <span className="featuredMoney">{productCount}</span>
           <span className="featuredMoneyRate">  16.5 <ArrowUpwardIcon className='featureIcon'/> </span>
         </div>
-        <span className="featuredSub">Compared to last Month</span>
+        <span className="featuredSub">Comparado con el mes anterior</span>
       </div>
       <div className="featuredItems">
-        <span className="featuredTitle">Costs</span>
+        <span className="featuredTitle">Cantidad de Direcciones</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$1.523.000</span>
+          <span className="featuredMoney">{addressCount}</span>
           <span className="featuredMoneyRate">  16.5 <ArrowUpwardIcon className='featureIcon'/> </span>
         </div>
-        <span className="featuredSub">Compared to last Month</span>
+        <span className="featuredSub">Comparado con el mes anterior</span>
       </div>
     </div>
   )
