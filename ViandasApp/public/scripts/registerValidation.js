e52198be5f4f0ddc13900ddfb7e6525c8d39069f
ArielHL <73503229+ArielHL@users.noreply.github.com>
2023-03-01 16:26:26 -0300

    Update registerValidation.js

--- ViandasApp/public/scripts/registerValidation.js
+++ ViandasApp/public/scripts/registerValidation.js
@@ -7,6 +7,22 @@ const passwordField = document.querySelector("[name=password]");
 const confirmPasswordField = document.querySelector("[name=confirmPassword]");
 
 
+const setErrors = (field, isError=true) => {
+    if (isError) {
+        field.classList.add("invalid");
+        field.classList.remove("valid");
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = `${field.name} es un campo requerido`;
+    } else {
+        field.classList.add("valid");
+        field.classList.remove("invalid");
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+}
+
+
+
 // Validacion de Nombre y Apellido
 nameField.addEventListener("blur", function(e){
     const field = e.target;
@@ -58,26 +74,25 @@ birthDate.addEventListener("blur", function(e){
     }
 });
 
+const validateEmailFormat = e => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (fieldValue.trim().length > 5 && !emailRegex.test(fieldValue)) {
+        setErrors(field);
+    } else {
+        setErrors(field, false);
+    }
+}
+
+
+
 //Validacion de Email
 //Expresiones Regulares https://www.w3schools.com/jsref/jsref_obj_regexp.asp       
 //https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-emailField.addEventListener("blur", function(e){
-    const field = e.target;
-    const fieldValue = e.target.value;
-    let reEmail  = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-       
-    if (fieldValue.length <= 2 && (!reEmail.test(emailField.value))) { 
-        field.nextElementSibling.classList.add("error");
-        field.classList.add("invalid");
-        field.classList.remove("valid");
-        field.nextElementSibling.innerText = "Debes escribir un email valido";
-    } else {
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerText = "";
-        field.classList.remove("invalid");
-        field.classList.add("valid");
-    }
-});
+// validacion email
+emailField.addEventListener("input",validateEmailFormat );
+emailField.addEventListener("blur", validateFieldRequired);
 
 //Validacion contraseña
 passwordField.addEventListener("blur", function(e){
