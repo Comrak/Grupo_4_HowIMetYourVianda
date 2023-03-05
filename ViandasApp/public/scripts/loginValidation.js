4a9f31f191875b3c5d890c4f064fcdc942857d85
ArielHL <73503229+ArielHL@users.noreply.github.com>
2023-03-05 18:56:00 -0300

    Update loginValidation.js

--- ViandasApp/public/scripts/loginValidation.js
+++ ViandasApp/public/scripts/loginValidation.js
@@ -1,5 +1,7 @@
 const emailField = document.querySelector("[name=email]");
 const passwordField = document.querySelector("[name=password]");
+let userEmails =[]
+
 
 const setErrors = (field, isError=true) => {
     if (isError) {
@@ -15,13 +17,37 @@ const setErrors = (field, isError=true) => {
     }
 }
 
-const validateFieldRequired = e => {
+
+
+
+const validateFieldRequired = async (e) => {
+
+    const response = await  fetch("http://localhost:3200/api/users")
+    const data = await response.json()
+    const dataToProcess = data.data
+
+    userEmails = dataToProcess.map(user => user.email)
+
+
     const field = e.target;
     const fieldValue = e.target.value;
-    if (fieldValue.trim().length === 0) {
-        setErrors(field);
-    } else {
+    // chequear si el campo no esta vacio
+    if (fieldValue.trim().length != 0 ) {
         setErrors(field, false);
+    // chequear si el email ya existe
+            if (!userEmails.includes(fieldValue)) {
+                
+                field.classList.add("invalid");
+                field.classList.remove("valid");
+                field.nextElementSibling.classList.add("error");
+                field.nextElementSibling.innerText = `${field.value} no existe en la base de datos`;
+    } else { 
+                setErrors(field,false);
+    }
+    } 
+    // si el campo esta vacio
+    else {
+                setErrors(field);
     }
 }
 
@@ -29,12 +55,14 @@ const validateEmailFormat = e => {
     const field = e.target;
     const fieldValue = e.target.value;
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if (fieldValue.trim().length > 5 && !emailRegex.test(fieldValue)) {
+    if (!emailRegex.test(fieldValue)) {
         setErrors(field);
     } else {
         setErrors(field, false);
     }
 }
+
+
 
 // validacion email
 emailField.addEventListener("input",validateEmailFormat );
@@ -43,14 +71,3 @@ emailField.addEventListener("blur", validateFieldRequired);
 //Validacion contraseña
 passwordField.addEventListener("blur", validateFieldRequired);
 
-// VALIDACION FRONT
-
-//● Login de usuarios 
-
-// ○ Email
-// ■ Obligatorio.
-// ■ Deberá ser válido.
-// ■ (Opcional) → Debe existir en la base.
-
-// ○ Contraseña
-// ■ Obligatoria.
