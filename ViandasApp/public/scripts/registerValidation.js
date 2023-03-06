b3efec3ab332ab91b0b860eaeebffa079b23cf77
ArielHL <73503229+ArielHL@users.noreply.github.com>
2023-03-05 22:51:55 -0300

    Update registerValidation.js

--- ViandasApp/public/scripts/registerValidation.js
+++ ViandasApp/public/scripts/registerValidation.js
@@ -6,195 +6,207 @@ const avatarField = document.querySelector("[name=avatar]");
 const passwordField = document.querySelector("[name=password]");
 const confirmPasswordField = document.querySelector("[name=confirmPassword]");
 
-
-const setErrors = (field, isError=true) => {
-    if (isError) {
-        field.classList.add("invalid");
-        field.classList.remove("valid");
-        field.nextElementSibling.classList.add("error");
-        field.nextElementSibling.innerText = `${field.name} es un campo requerido`;
-    } else {
-        field.classList.add("valid");
-        field.classList.remove("invalid");
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerText = "";
-    }
-}
-
-
+const setErrors = (field, isError = true) => {
+  if (isError) {
+    field.classList.add("invalid");
+    field.classList.remove("valid");
+    field.nextElementSibling.classList.add("error");
+    field.nextElementSibling.innerText = `${field.name} es un campo requerido`;
+  } else {
+    field.classList.add("valid");
+    field.classList.remove("invalid");
+    field.nextElementSibling.classList.remove("error");
+    field.nextElementSibling.innerText = "";
+  }
+};
 
 // Validacion de Nombre y Apellido
-nameField.addEventListener("blur", function(e){
-    const field = e.target;
-    const fieldValue = e.target.value;
-    if (fieldValue.length <= 2) {
-        field.nextElementSibling.classList.add("error");
-        field.classList.add("invalid");
-        field.classList.remove("valid");
-        field.nextElementSibling.innerText = "Debes escribir tu Nombre y Apellido";
-    } else {
-        field.classList.remove("invalid");
-        field.classList.add("valid");
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerText = "";
-    }
+nameField.addEventListener("blur", function (e) {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  if (fieldValue.length <= 2) {
+    field.nextElementSibling.classList.add("error");
+    field.classList.add("invalid");
+    field.classList.remove("valid");
+    field.nextElementSibling.innerText = "Debes escribir tu Nombre y Apellido";
+  } else {
+    field.classList.remove("invalid");
+    field.classList.add("valid");
+    field.nextElementSibling.classList.remove("error");
+    field.nextElementSibling.innerText = "";
+  }
 });
-
 
 // Validacion de Numero de telefono
-phoneField.addEventListener("blur", function(e){
-    const field = e.target;
-    const fieldValue = e.target.value;
-    if (fieldValue.length <= 2) {
-        field.nextElementSibling.classList.add("error");
-        field.classList.add("invalid");
-        field.classList.remove("valid");
-        field.nextElementSibling.innerText = "Debes escribir un Telefono";
-    } else {
-        field.nextElementSibling.classList.remove("error");
-        field.classList.remove("invalid");
-        field.classList.add("valid");
-        field.nextElementSibling.innerText = "";
-    }
+phoneField.addEventListener("blur", function (e) {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  if (fieldValue.length <= 2) {
+    field.nextElementSibling.classList.add("error");
+    field.classList.add("invalid");
+    field.classList.remove("valid");
+    field.nextElementSibling.innerText = "Debes escribir un Telefono";
+  } else {
+    field.nextElementSibling.classList.remove("error");
+    field.classList.remove("invalid");
+    field.classList.add("valid");
+    field.nextElementSibling.innerText = "";
+  }
 });
 
-birthDate.addEventListener("blur", function(e){
-   const field = e.target;
-    const fieldValue = e.target.value;
-    if ( fieldValue === '') {
-        field.classList.add("invalid");
-        field.classList.remove("valid");
-        field.nextElementSibling.classList.add("error");
-        field.nextElementSibling.innerText = "Debes escribir una fecha";
-    } else {
-        field.classList.remove("invalid");
-        field.classList.add("valid");
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerText = "";
-    }
+birthDate.addEventListener("blur", function (e) {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  if (fieldValue === "") {
+    field.classList.add("invalid");
+    field.classList.remove("valid");
+    field.nextElementSibling.classList.add("error");
+    field.nextElementSibling.innerText = "Debes escribir una fecha";
+  } else {
+    field.classList.remove("invalid");
+    field.classList.add("valid");
+    field.nextElementSibling.classList.remove("error");
+    field.nextElementSibling.innerText = "";
+  }
 });
 
+const validateFieldRequired = async (e) => {
+  const response = await fetch("http://localhost:3200/api/users");
+  const data = await response.json();
+  const dataToProcess = data.data;
 
+  userEmails = dataToProcess.map((user) => user.email);
 
-
-const validateFieldRequired = e => {
-    const field = e.target;
-    const fieldValue = e.target.value;
-    if (fieldValue.trim().length === 0) {
-        setErrors(field);
+  const field = e.target;
+  const fieldValue = e.target.value;
+  // chequear si el campo no esta vacio
+  if (fieldValue.trim().length != 0) {
+    setErrors(field, false);
+    // chequear si el email ya existe
+    if (userEmails.includes(fieldValue)) {
+      field.classList.add("invalid");
+      field.classList.remove("valid");
+      field.nextElementSibling.classList.add("error");
+      field.nextElementSibling.innerText = `${field.value} ya es un usuario registrado`;
     } else {
-        setErrors(field, false);
+      setErrors(field, false);
     }
-}
-const validateEmailFormat = e => {
-    const field = e.target;
-    const fieldValue = e.target.value;
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if (fieldValue.trim().length > 5 && !emailRegex.test(fieldValue)) {
-        setErrors(field);
-    } else {
-        setErrors(field, false);
-    }
-}
+  }
+  // si el campo esta vacio
+  else {
+    setErrors(field);
+  }
+};
+
+const validateEmailFormat = (e) => {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  const emailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (fieldValue.trim().length > 5 && !emailRegex.test(fieldValue)) {
+    setErrors(field);
+  } else {
+    setErrors(field, false);
+  }
+};
 
 //Validacion de Email
-//Expresiones Regulares https://www.w3schools.com/jsref/jsref_obj_regexp.asp       
+//Expresiones Regulares https://www.w3schools.com/jsref/jsref_obj_regexp.asp
 //https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 // validacion email
-emailField.addEventListener("input",validateEmailFormat );
+emailField.addEventListener("input", validateEmailFormat);
 emailField.addEventListener("blur", validateFieldRequired);
 
 //Validacion contraseña
-passwordField.addEventListener("blur", function(e){
-    const field = e.target;
-    const fieldValue = e.target.value;
-    if (fieldValue.length <= 7) {
-        field.nextElementSibling.classList.add("error");
-        field.classList.add("invalid");
-        field.classList.remove("valid");
-        field.nextElementSibling.innerText = "Debes tener al menos 8 caracteres";
-    } else {
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerText = "";
-        field.classList.remove("invalid");
-        field.classList.add("valid");
-    }
+passwordField.addEventListener("blur", function (e) {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  if (fieldValue.length <= 7) {
+    field.nextElementSibling.classList.add("error");
+    field.classList.add("invalid");
+    field.classList.remove("valid");
+    field.nextElementSibling.innerText = "Debes tener al menos 8 caracteres";
+  } else {
+    field.nextElementSibling.classList.remove("error");
+    field.nextElementSibling.innerText = "";
+    field.classList.remove("invalid");
+    field.classList.add("valid");
+  }
 });
 
 //Validacion contraseña
 
 //Cantidad de caracteres
-confirmPasswordField.addEventListener("blur", function(e){
-    const field = e.target;
-    const fieldValue = e.target.value;
-    if (fieldValue.length <= 7) {
-        field.nextElementSibling.classList.add("error");
-        field.classList.add("invalid");
-        field.classList.remove("valid");
-        field.nextElementSibling.innerText = "Debes tener al menos 8 caracteres";
-    } else if (fieldValue !== passwordField.value) {
-        field.nextElementSibling.classList.add("error");
-        field.classList.add("invalid");
-        field.classList.remove("valid");
-        field.nextElementSibling.innerText = "Las contraseñas no coinciden";
-    } else {
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerText = "";
-        field.classList.remove("invalid");
-        field.classList.add("valid");
-    }
+confirmPasswordField.addEventListener("blur", function (e) {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  if (fieldValue.length <= 7) {
+    field.nextElementSibling.classList.add("error");
+    field.classList.add("invalid");
+    field.classList.remove("valid");
+    field.nextElementSibling.innerText = "Debes tener al menos 8 caracteres";
+  } else if (fieldValue !== passwordField.value) {
+    field.nextElementSibling.classList.add("error");
+    field.classList.add("invalid");
+    field.classList.remove("valid");
+    field.nextElementSibling.innerText = "Las contraseñas no coinciden";
+  } else {
+    field.nextElementSibling.classList.remove("error");
+    field.nextElementSibling.innerText = "";
+    field.classList.remove("invalid");
+    field.classList.add("valid");
+  }
 });
-
 
 //Validacion Imagen
 
 //Campo completo
-avatarField.addEventListener("blur", function(e){
-    const field = e.target;
-     const fieldValue = e.target.value;
-     if ( fieldValue === '') {
-         field.nextElementSibling.classList.add("error");
-         field.nextElementSibling.innerText = "Debes subir una imágen";
-         field.classList.add("invalid");
-         field.classList.remove("valid");
-     } else {
-         field.nextElementSibling.classList.remove("error");
-         field.nextElementSibling.innerText = "";
-     }
- });
- 
+avatarField.addEventListener("blur", function (e) {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  if (fieldValue === "") {
+    field.nextElementSibling.classList.add("error");
+    field.nextElementSibling.innerText = "Debes subir una imágen";
+    field.classList.add("invalid");
+    field.classList.remove("valid");
+  } else {
+    field.nextElementSibling.classList.remove("error");
+    field.nextElementSibling.innerText = "";
+  }
+});
+
 //Tipo de archivos
-avatarField.addEventListener("change", function(e){
-    const field = e.target;
-    const fileExtension = e.target.files[0].name.split(".").pop().toLowerCase();
-    const allowedExt = ["jpg", "jpeg", "png", "gif"];
-    if (!allowedExt.includes(fileExtension)) {
-        field.nextElementSibling.classList.add("error");        
-        field.classList.add("invalid");
-        field.classList.remove("valid");
-        field.nextElementSibling.innerText = `Las extensiones permitidas son ${allowedExt.join(", ")}`;
-    } else {
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerText = "";
-        field.classList.remove("invalid");
-        field.classList.add("valid");
-    }
-  });
+avatarField.addEventListener("change", function (e) {
+  const field = e.target;
+  const fileExtension = e.target.files[0].name.split(".").pop().toLowerCase();
+  const allowedExt = ["jpg", "jpeg", "png", "gif"];
+  if (!allowedExt.includes(fileExtension)) {
+    field.nextElementSibling.classList.add("error");
+    field.classList.add("invalid");
+    field.classList.remove("valid");
+    field.nextElementSibling.innerText = `Las extensiones permitidas son ${allowedExt.join(
+      ", "
+    )}`;
+  } else {
+    field.nextElementSibling.classList.remove("error");
+    field.nextElementSibling.innerText = "";
+    field.classList.remove("invalid");
+    field.classList.add("valid");
+  }
+});
 
-
-        // 4. Validaciones del front-end
-        // ● Registro de usuarios
-        // ○ Nombre y apellido
-        // ■ Obligatorio.
-        // ■ Deberá tener al menos 2 caracteres.
-        // ○ Email
-        // ■ Obligatorio.
-        // ■ Deberá ser válido.
-        // ■ (Opcional) → No puede repetirse con los e-mails ya registrados.
-        // ○ Contraseña
-        // ■ Obligatoria.
-        // ■ Deberá tener al menos 8 caracteres.
-        // ■ (Opcional) → Deberá tener letras mayúsculas, minúsculas, un número y un carácter especial.
-        // ○ Imagen
-        // ■ Deberá ser un archivo válido (JPG, JPEG, PNG, GIF).
-
+// 4. Validaciones del front-end
+// ● Registro de usuarios
+// ○ Nombre y apellido
+// ■ Obligatorio.
+// ■ Deberá tener al menos 2 caracteres.
+// ○ Email
+// ■ Obligatorio.
+// ■ Deberá ser válido.
+// ■ (Opcional) → No puede repetirse con los e-mails ya registrados.
+// ○ Contraseña
+// ■ Obligatoria.
+// ■ Deberá tener al menos 8 caracteres.
+// ■ (Opcional) → Deberá tener letras mayúsculas, minúsculas, un número y un carácter especial.
+// ○ Imagen
+// ■ Deberá ser un archivo válido (JPG, JPEG, PNG, GIF).
